4b85c6cc8fa4f653f44048b845939471180d21c2
miracleyoo <miracleyoo@163.com>
2019-09-22 20:15:18 -0400

    Remove the now ignored directory "some-directory"

--- project3/assign1part3.hh
+++
@@ -1,5 +0,0 @@
-extern int bits_of_page;
-extern int bits_of_virtual_memory;
-extern int bits_of_physical_memory;
-extern int number_of_virtual_memory;
-extern int number_of_physical_memory;
--- project3/pagetable.hh
+++
@@ -1,6 +0,0 @@
-// Author: Miracleyoo
-// E-mail: mirakuruyoo@gmail.com
-// Date: 2019.09.22
-
-extern int page_fault_counter;
-uint64_t HandleAllocation(uint64_t address);
